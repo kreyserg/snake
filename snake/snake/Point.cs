f27d8a360d79496a75ff578d19603b03528b95d6
Kreyserg <kreyserg@yandex.ru>
2018-07-26 00:37:10 +0600

    Создаем конструкторы

--- snake/snake/Point.cs
+++ snake/snake/Point.cs
@@ -11,5 +11,22 @@ namespace snake
         public int x;
         public int y;
         public char sym;
+
+        public Point()
+        {
+        }
+
+        public Point(int x, int y, char sym)
+        {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
+        }
     }
 }
